4409285620d9be3ecee6fac2224d7a76408f262a
Matt Grochocinski <matt.grochocinski@gmail.com>
2019-08-05 11:50:21 -0500

    updated sso script

--- heroku_sso.sh
+++ heroku_sso.sh
@@ -1,20 +1,28 @@
 #!/bin/bash
 
-if [[ -z "$@" ]]; then
-    echo >&2 "You must supply an argument!"
+error () {
+	echo >&2 "Missing parameter!"
+    echo >&2 "[-up] to enable SSO support."
+    echo >&2 "[-down] to disable SSO support."
     exit 1
-fi
+}
 
-enable()
-{
+enable () {
 	git config --global url."https://git.heroku.com/".insteadOf "git@heroku.com:"
 	echo "Heroku configured for HTTPS. You may enable SSO."
 	exit 0
 }
 
-disable()
-{
+disable () {
 	git config --global --unset url.https://git.heroku.com/.insteadOf
 	echo "Heroku configured for SSH. You may now disable SSO."
 	exit 0
 }
+
+if [ "$1" == "-up" ]; then
+	enable
+elif [ "$1" == "-down" ]; then
+	disable
+else
+	error
+fi

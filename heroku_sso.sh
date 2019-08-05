d19aa44aa8aad23603ad7d48638c5d410002f008
Matt Grochocinski <matt.grochocinski@gmail.com>
2019-08-05 11:33:25 -0500

    first modification

--- heroku_sso.sh
+++ heroku_sso.sh
@@ -8,11 +8,13 @@ fi
 enable()
 {
 	git config --global url."https://git.heroku.com/".insteadOf "git@heroku.com:"
+	echo "Heroku configured for HTTPS. You may enable SSO."
 	exit 0
 }
 
 disable()
 {
 	git config --global --unset url.https://git.heroku.com/.insteadOf
+	echo "Heroku configured for SSH. You may now disable SSO."
 	exit 0
 }

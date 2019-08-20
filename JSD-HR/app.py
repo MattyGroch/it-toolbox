3744303e90ef8cd6a5ba480f452237cbca1bc3bc
Matt Grochocinski <matt.grochocinski@gmail.com>
2019-08-19 23:50:08 -0500

    WIP - initial jsd-hr commit

---
+++ JSD-HR/app.py
@@ -0,0 +1,59 @@
+from chalice import Chalice
+import os
+import json
+import requests
+import functions
+
+app = Chalice(app_name='JSD-HR')
+# set to True if debugging
+app.debug = False
+
+
+@app.route('/onboard', methods=['POST'])
+def index():
+    try:
+        functions.jira_parser(app.current_request.json_body)
+        functions.send_email(
+            "helpdesk@snapsheet.me",
+            functions.format_email("it")
+            )
+        return {'status': "Success."}
+    except:
+        return {'status': "An error ocurred."}
+
+
+@app.route('/offboard', methods=['POST'])
+def index():
+    try:
+        functions.jira_parser(app.current_request.json_body)
+        return {'status': "Not setup."}
+    except:
+        return {'status': "An error ocurred."}
+
+
+@app.route('/infochange', methods=['POST'])
+def index():
+    try:
+        return {'status': "Not setup."}
+    except:
+        return {'status': "An error ocurred."}
+
+# The view function above will return {"hello": "world"}
+# whenever you make an HTTP GET request to '/'.
+#
+# Here are a few more examples:
+#
+# @app.route('/hello/{name}')
+# def hello_name(name):
+#    # '/hello/james' -> {"hello": "james"}
+#    return {'hello': name}
+#
+# @app.route('/users', methods=['POST'])
+# def create_user():
+#     # This is the JSON body the user sent in their POST request.
+#     user_as_json = app.current_request.json_body
+#     # We'll echo the json body back to the user in a 'user' key.
+#     return {'user': user_as_json}
+#
+# See the README documentation for more examples.
+#
---
+++ JSD-HR/functions.py
@@ -0,0 +1,76 @@
+import smtplib
+import ssl
+import os
+import request
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+
+
+def format_email(template):
+    if template == "employee":
+        message = """Subject: Hi there
+
+This message is for the employee."""
+        return message
+    elif template == "payroll":
+        message = """Subject: Hi there
+
+This message is for Cognos."""
+        return message
+    elif template == "manager":
+        message = """Subject: Hi there
+
+This message is for the manager."""
+        return message
+    elif template == "it":
+        message = """Subject: Hi there
+
+This message is for IT."""
+        return message
+    else:
+        return "Not a valid template."
+
+
+def send_email(toaddress, message):
+    gmail_user = os.environ['EMAIL_ACCT']
+    gmail_password = os.environ['EMAIL_PWD']
+    port = 465  # For SSL
+
+    # Create a secure SSL context
+    context = ssl.create_default_context()
+
+    with smtplib.SMTP_SSL("smtp.gmail.com", port, context=context) as server:
+        server.login(gmail_user, gmail_password)
+        server.sendmail(gmail_user, toaddress, message)
+
+
+def jira_parser():
+    try:
+    # parse request payload data
+        issue_id = request['issue']['id']
+        issue_key = request['issue']['key']
+    # parse issue fields
+        issue_priority = request['issue']['fields']['priority']['name']
+        issue_creator_username = request['issue']['fields']['creator']['name']
+        issue_creator_displayname = request['issue']['fields']['creator']['displayName']
+        issue_reporter_username = request['issue']['fields']['reporter']['name']
+        issue_reporter_displayname = request['issue']['fields']['reporter']['displayName']
+        issue_type = request['issue']['fields']['issuetype']['name']
+        issue_project = request['issue']['fields']['project']['key']
+        issue_summary = request['issue']['fields']['summary']
+        issue_description = request['issue']['fields']['description']
+    except:
+        return "Error with Jira ticket info."
+    return {
+        issue_id,
+        issue_key,
+        issue_priority,
+        issue_creator_username,
+        issue_creator_displayname,
+        issue_reporter_username,
+        issue_reporter_displayname,
+        issue_type,
+        issue_project,
+        issue_summary,
+        issue_description
+        }

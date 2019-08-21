fafa2137ab582b89255fb120b566650df0d481e2
Matt Grochocinski <matt.grochocinski@gmail.com>
2019-08-21 12:44:35 -0500

    WIP - sanity check

--- JSD-HR/app.py
+++ JSD-HR/app.py
@@ -8,11 +8,50 @@ app = Chalice(app_name='JSD-HR')
 # set to True if debugging
 app.debug = False
 
+class JiraIssue:
+    def __init__(self, request):
+        self.id = request['issue']['id']
+        self.key = request['issue']['key']
+        self.priority = request['issue']['fields']['priority']['name']
+        self.creator_username = request['issue']['fields']['creator']['name']
+        self.creator_displayname = request['issue']['fields']['creator']['displayName']
+        self.reporter_username = request['issue']['fields']['reporter']['name']
+        self.reporter_displayname = request['issue']['fields']['reporter']['displayName']
+        self.type = request['issue']['fields']['issuetype']['name']
+        self.project = request['issue']['fields']['project']['key']
+        self.summary = request['issue']['fields']['summary']
+        self.description = request['issue']['fields']['description']
 
-@app.route('/onboard/{issue}', methods=['POST'])
+
+class Employee:
+    def __init__(self, request):
+        fields = request['issue']['fields']
+        self.wage_type = fields['customfield_10161']['value']
+        self.wage_amount = fields['customfield_10162']
+        self.email = fields['customfield_10163']
+        self.phone = fields['customfield_10164']
+        self.flsa = fields['customfield_10165']['value']
+        self.address1 = fields['customfield_10150']
+        self.address2 = fields['customfield_10151']
+        self.city = fields['customfield_10152']
+        self.state = fields['customfield_10153']['value']
+        self.zip = fields['customfield_10154']
+        self.department = fields['customfield_10149']['value']
+        self.firstname = fields['customfield_10142']
+        self.lastname = fields['customfield_10143']
+        self.preferredname = fields['customfield_10144']
+        self.manager = fields['customfield_10145']['displayName']
+        self.location = fields['customfield_10146']['value']
+
+
+@app.route('/onboard', methods=['POST'])
 def index():
     try:
-        onboard_user(issue)
+        payload = app.current_request.json_body
+        issue = JiraIssue(payload)
+        employee = Employee(payload)
+        functions.send_email("helpdesk@snapsheet.me",functions.generate_email("it"))
+        functions.send_email("payroll@email.com",functions.generate_email("payroll"))
         return {'status': "Success."}
     except:
         return {'status': "An error ocurred."}
@@ -21,7 +60,6 @@ def index():
 @app.route('/offboard', methods=['POST'])
 def index():
     try:
-        functions.jira_parser(app.current_request.json_body)
         return {'status': "Not setup."}
     except:
         return {'status': "An error ocurred."}
@@ -33,23 +71,3 @@ def index():
         return {'status': "Not setup."}
     except:
         return {'status': "An error ocurred."}
-
-# The view function above will return {"hello": "world"}
-# whenever you make an HTTP GET request to '/'.
-#
-# Here are a few more examples:
-#
-# @app.route('/hello/{name}')
-# def hello_name(name):
-#    # '/hello/james' -> {"hello": "james"}
-#    return {'hello': name}
-#
-# @app.route('/users', methods=['POST'])
-# def create_user():
-#     # This is the JSON body the user sent in their POST request.
-#     user_as_json = app.current_request.json_body
-#     # We'll echo the json body back to the user in a 'user' key.
-#     return {'user': user_as_json}
-#
-# See the README documentation for more examples.
-#
--- JSD-HR/functions.py
+++ JSD-HR/functions.py
@@ -1,21 +1,31 @@
 import smtplib
 import ssl
 import os
-import request
-from email.mime.text import MIMEText
+import sys
+import requests
 from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from email.mime.base import MIMEBase
+from email import encoders
+from dotenv import load_dotenv
+load_dotenv()
 
-
-def format_email(template):
+def generate_email(template):
     if template == "employee":
         message = """Subject: Hi there
 
-This message is for the employee."""
+This message is for the employee.
+
+Best,
+Snapsheet HR"""
         return message
     elif template == "payroll":
         message = """Subject: Hi there
 
-This message is for Cognos."""
+This is an email.
+
+Best,
+Snapsheet HR"""
         return message
     elif template == "manager":
         message = """Subject: Hi there
@@ -28,12 +38,13 @@ This message is for the manager."""
 This message is for IT."""
         return message
     else:
-        return "Not a valid template."
+        print("Template failed to generate.")
+        sys.exit(1)
 
 
 def send_email(toaddress, message):
-    gmail_user = os.environ['EMAIL_ACCT']
-    gmail_password = os.environ['EMAIL_PWD']
+    gmail_user = os.getenv("EMAIL_ACCT")
+    gmail_password = os.getenv("EMAIL_PWD")
     port = 465  # For SSL
 
     # Create a secure SSL context
@@ -44,38 +55,36 @@ def send_email(toaddress, message):
         server.sendmail(gmail_user, toaddress, message)
 
 
-def jira_parser():
-    try:
-    # parse request payload data
-        issue_id = request['issue']['id']
-        issue_key = request['issue']['key']
-    # parse issue fields
-        issue_priority = request['issue']['fields']['priority']['name']
-        issue_creator_username = request['issue']['fields']['creator']['name']
-        issue_creator_displayname = request['issue']['fields']['creator']['displayName']
-        issue_reporter_username = request['issue']['fields']['reporter']['name']
-        issue_reporter_displayname = request['issue']['fields']['reporter']['displayName']
-        issue_type = request['issue']['fields']['issuetype']['name']
-        issue_project = request['issue']['fields']['project']['key']
-        issue_summary = request['issue']['fields']['summary']
-        issue_description = request['issue']['fields']['description']
-    except:
-        return "Error with Jira ticket info."
-
-    jira_dict = {
-        'id': issue_id,
-        'key': issue_key,
-        'priority': issue_priority,
-        'createdby_user': issue_creator_username,
-        'createdby_dispname': issue_creator_displayname,
-        'reporter_user': issue_reporter_username,
-        'reporter_dispname': issue_reporter_displayname,
-        'type': issue_type,
-        'project': issue_project,
-        'summary': issue_summary,
-        'description': issue_description
-    }
-
-    return jira_dict
-
-def onboard_user():
+# def jira_parser(request):
+#     try:
+#     # parse request payload data
+#         issue_id = request['issue']['id']
+#         issue_key = request['issue']['key']
+#     # parse issue fields
+#         issue_priority = request['issue']['fields']['priority']['name']
+#         issue_creator_username = request['issue']['fields']['creator']['name']
+#         issue_creator_displayname = request['issue']['fields']['creator']['displayName']
+#         issue_reporter_username = request['issue']['fields']['reporter']['name']
+#         issue_reporter_displayname = request['issue']['fields']['reporter']['displayName']
+#         issue_type = request['issue']['fields']['issuetype']['name']
+#         issue_project = request['issue']['fields']['project']['key']
+#         issue_summary = request['issue']['fields']['summary']
+#         issue_description = request['issue']['fields']['description']
+#     except:
+#         return "Error with Jira ticket info."
+#
+#     jira_dict = {
+#         'id': issue_id,
+#         'key': issue_key,
+#         'priority': issue_priority,
+#         'createdby_user': issue_creator_username,
+#         'createdby_dispname': issue_creator_displayname,
+#         'reporter_user': issue_reporter_username,
+#         'reporter_dispname': issue_reporter_displayname,
+#         'type': issue_type,
+#         'project': issue_project,
+#         'summary': issue_summary,
+#         'description': issue_description
+#     }
+#
+#     return jira_dict
